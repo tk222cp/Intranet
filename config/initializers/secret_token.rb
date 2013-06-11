cd828a9eac86a7cea0c7a58cf3711379f13c6e69
Tommy Karlsson <tommy.sf.karlsson@gmail.com>
2013-06-11 22:55:12 +0200

    First commit
    Projektet är skapat

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Intranet::Application.config.secret_token = '93ac088d0ed1a88fc95e7703317d7e377ae6b761c11d24961bbfce44ccbf3bca22acf7fbd7d38c13358f6b2272d47690957eb678c686d01fac3bac6674c90777'
